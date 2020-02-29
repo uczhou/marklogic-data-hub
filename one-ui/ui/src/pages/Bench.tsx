220e95982fb2ae781d2493c7d98a2f33f9746ffd
wooldridge <mike@hellion.org>
2020-02-28 16:01:06 -0800

    DHFPROD-3922: Poll when running flows

--- one-ui/ui/src/pages/Bench.tsx
+++ one-ui/ui/src/pages/Bench.tsx
@@ -110,17 +110,53 @@ const Bench: React.FC = () => {
         }
     }
 
-    const runStep = async (stepName) => {
+    // Polling function
+    function poll(fn, timeout, interval) {
+        var endTime = Number(new Date()) + (timeout || 20000);
+        interval = interval || 500;
+        var checkCondition = function(resolve, reject) { 
+            var promise = fn();
+            promise.then( function(response){
+                console.log('Flow status: ', response.data.jobStatus);
+                if(response.data.jobStatus === 'finished') {
+                    resolve(response.data.jobStatus);
+                }
+                // TODO Get rid of timeout
+                else if (Number(new Date()) < endTime) {
+                    setTimeout(checkCondition, interval, resolve, reject);
+                }
+                else {
+                    reject(new Error('timed out for ' + fn + ': ' + arguments));
+                }
+            });
+        };
+        return new Promise(checkCondition);
+    }
+
+    const runStep = async (flowId, stepId) => {
         try {
             setIsLoading(true);
-            //let response = await axios.delete(`/api/flows/${name}`);
-            //if (response.status === 200) {
-                console.log('POST run step', stepName);
-                setIsLoading(false);
-            //} 
+            let body = [stepId];
+            let response = await axios.post('/api/flows/' + flowId + '/run', body);
+            if (response.status === 200) {
+                console.log('Flow started: ', flowId);
+                let interval = 500; // in milliseconds
+                let jobId = response.data.jobId;
+                // TODO Check for errors with timeout instead of using setTimeout
+                await setTimeout( function(){ 
+                    poll(function() {
+                        return axios.get('/api/jobs/' + jobId);
+                    }, 20000, interval).then(function() {
+                        console.log('Flow complete: ' + flowId);
+                        setIsLoading(false);
+                    }).catch(function() {
+                        console.log('Flow timeout');
+                        setIsLoading(false);
+                    });
+                }, interval);
+            } 
         } catch (error) {
-            //let message = error.response.data.message;
-            console.log('Error running step', error);
+            console.log('Error in runStep', error);
             setIsLoading(false);
         }
     }
